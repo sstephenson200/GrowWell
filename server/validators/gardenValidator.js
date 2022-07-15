5a6c4dccd45842cdfd88c9f7bfbb4a9dfb1d4155
Sophie <sstephenson200@hotmail.co.uk>
2022-07-15 16:53:19 +0100

    Updating Plot Number Logic

--- server/validators/gardenValidator.js
+++ server/validators/gardenValidator.js
@@ -20,8 +20,6 @@ async function checkExistingGardenName(name, user_id) {
 //Check plot_number is valid
 function checkValidPlotNumber(gardenSize, plot_number) {
 
-    plot_number -= 1;
-
     if (plot_number >= 0 && plot_number < gardenSize) {
         return true;
     }
