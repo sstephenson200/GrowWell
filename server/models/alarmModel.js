d370c803d29648b4758f7f89584ea476e292aceb
Sophie <sstephenson200@hotmail.co.uk>
2022-07-28 17:23:45 +0100

    Adding isParent flag to alarm

--- server/models/alarmModel.js
+++ server/models/alarmModel.js
@@ -7,6 +7,7 @@ const alarmSchema = new mongoose.Schema({
     due_date: { type: Date, required: true },
     garden_id: { type: mongoose.Schema.Types.ObjectId, ref: "garden", default: null },
     plot_number: { type: Number, default: null },
+    isParent: { type: Boolean, default: false },
     parent: { type: mongoose.Schema.Types.ObjectId, ref: "alarm", default: null },
     completion_status: { type: Boolean, default: false },
     active_status: { type: Boolean, default: true }
