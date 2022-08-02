8f1a23714661667a7433dd4ba360ddf1afd4b958
Sophie <sstephenson200@hotmail.co.uk>
2022-08-02 20:56:10 +0100

    Correcting file naming conventions

--- client/App.js
+++ client/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
 
-import Tabs from './src/app/navigation/tabs';
+import Tabs from './src/app/navigation/Tabs';
 import Notification from './src/app/notifications/PushNotification';
 import { AuthContextProvider } from './src/app/context/AuthContext';
 
--- client/src/app/navigation/tabs.js
+++ client/src/app/navigation/tabs.js
@@ -4,7 +4,7 @@ import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 import AuthContext from '../context/AuthContext';
 
-import StackNavigator from './stackNavigator';
+import StackNavigator from './StackNavigator';
 import CalendarScreen from "../screens/CalendarScreen";
 import NewNoteScreen from "../screens/NewNoteScreen";
 import GardenScreen from "../screens/GardenScreen";
@@ -204,115 +204,6 @@ const Tabs = (route) => {
 
         );
     }
-
-    // return (
-
-    //     <Tab.Navigator
-    //         initialRouteName="Garden"
-    //         screenOptions={({ route }) => ({
-    //             tabBarShowLabel: false,
-    //             tabBarStyle: getTabBarVisibility(route, loggedIn)
-    //         })}
-    //     >
-    //         {loggedIn === false ? (
-    //             <Tab.Screen
-    //                 name="StackNavigator"
-    //                 component={StackNavigator}
-
-    //                 options={({ route }) => ({
-    //                     tabBarItemStyle: { display: "none" },
-    //                     headerShown: false,
-    //                 })}
-    //             />
-    //         ) : (
-    //             <>
-    //                 <Tab.Screen
-    //                     name="Calendar"
-    //                     component={CalendarScreen}
-    //                     options={{
-    //                         tabBarIcon: () => {
-    //                             return (
-    //                                 <FontAwesome name="calendar" size={40} color="white" />
-    //                             )
-    //                         },
-    //                         headerShown: false
-    //                     }}
-    //                 />
-
-    //                 <Tab.Screen
-    //                     name="Notes"
-    //                     component={NewNoteScreen}
-    //                     options={{
-    //                         tabBarIcon: () => {
-    //                             return <FontAwesome name="book" size={40} color="white" />
-    //                         },
-    //                         headerShown: false
-    //                     }}
-    //                 />
-
-    //                 <Tab.Screen
-    //                     name="Garden"
-    //                     component={GardenScreen}
-    //                     options={{
-    //                         tabBarIcon: () => {
-    //                             return <FontAwesome name="home" size={40} color="white" />
-    //                         },
-    //                         headerShown: false
-    //                     }}
-    //                 />
-
-    //                 <Tab.Screen
-    //                     name="Alarms"
-    //                     component={AlarmScreen}
-    //                     options={{
-    //                         tabBarIcon: () => {
-    //                             return <Ionicons name="ios-alarm" size={40} color="white" />
-    //                         },
-    //                         headerShown: false
-    //                     }}
-    //                 />
-
-    //                 <Tab.Screen
-    //                     name="Plants"
-    //                     component={PlantListScreen}
-    //                     options={{
-    //                         tabBarIcon: () => {
-    //                             return <Ionicons name="leaf" size={40} color="white" />
-    //                         },
-    //                         headerShown: false
-    //                     }}
-    //                 />
-    //             </>
-    //         )}
-
-    //     </Tab.Navigator >
-
-    // );
-    // } else {
-    //     return (
-
-    //         <Tab.Navigator
-    //             initialRouteName="StackNavigator"
-    //             screenOptions={({ route }) => ({
-    //                 tabBarShowLabel: false,
-    //                 tabBarStyle: getTabBarVisibility(route, loggedIn)
-    //             })}
-    //         >
-
-    //             <Tab.Screen
-    //                 name="StackNavigator"
-    //                 component={StackNavigator}
-
-    //                 options={({ route }) => ({
-    //                     tabBarItemStyle: { display: "none" },
-    //                     headerShown: false,
-    //                 })}
-    //             />
-
-    //         </Tab.Navigator >
-    //     );
-    // }
-
 }
 
 export default Tabs;
