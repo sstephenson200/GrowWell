7e3127a36a28a8f284b653725736f39176b1d29b
Sophie <sstephenson200@hotmail.co.uk>
2022-08-17 21:18:32 +0100

    App Unit Tests

---
+++ client/test/App.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { create } from "react-test-renderer";
+
+import App from "../App";
+
+describe("<App />", () => {
+
+    it("has 1 child", () => {
+        const tree = create(<App />).toJSON();
+        expect(tree.children.length).toBe(1);
+    });
+
+});
